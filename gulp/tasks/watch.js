6669ca76594ea7e06cba92bb57a4699d7c66a070
andreyivanov86@gmail.com <Andreyivanov86@gmail.com>
2017-04-16 10:57:42 -0400

    error handling in gulp

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,27 @@
+
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+var browserSync = require('browser-sync').create();
+// watch task for gulp-watch triggered by html task
+gulp.task('watch', function(){
+	//sets up automatic browser refresh on file save
+	browserSync.init({
+		notify: false,
+		server: {          //points to the local server that runs the index.html
+			baseDir: 'app'
+		}
+	});
+	//watching saved ch anges in index.html file
+	watch('./app/index.html', function(){
+		browserSync.reload();
+	});
+	//watchin saved in folder styles
+	watch('./app/assets/styles/**/*.css', function(){
+		gulp.start('cssInject');
+	});
+}); 
+//handles the browser automatic refresh 
+gulp.task('cssInject', ['styles'], function(){
+	return gulp.src('./app/temp/styles/styles.css')
+	.pipe(browserSync.stream());
+})
--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,8 @@
-var gulp = require('gulp');
-var watch = require('gulp-watch');
-var postcss = require('gulp-postcss');
-var autoprefixer = require('autoprefixer');
-var cssvars = require('postcss-simple-vars');
-var nested = require('postcss-nested');
-var cssImport = require('postcss-import');
-var browserSync = require('browser-sync').create();
 
-//Default Taske for Gulp
-gulp.task('default', function(){
-	console.log("Gulp task created.");
-});
-// html task for gulp
-gulp.task('html', function(){
-	console.log("Imagine something useful done.");
-});
-//styles task 
-gulp.task('styles', function(){
-	return gulp.src('./app/assets/styles/styles.css')
-	.pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
-	.pipe(gulp.dest('./app/temp/styles'));
-});
-// watch task for gulp-watch triggered by html task
-gulp.task('watch', function(){
-	//sets up automatic browser refresh on file save
-	browserSync.init({
-		notify: false,
-		server: {          //points to the local server that runs the index.html
-			baseDir: 'app'
-		}
-	});
-	//watching saved ch anges in index.html file
-	watch('./app/index.html', function(){
-		browserSync.reload();
-	});
-	//watchin saved in folder styles
-	watch('./app/assets/styles/**/*.css', function(){
-		gulp.start('cssInject');
-	});
-}); 
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
 
-gulp.task('cssInject', ['styles'], function(){
-	return gulp.src('./app/temp/styles/styles.css')
-	.pipe(browserSync.stream());
-})
+
+
+
+
