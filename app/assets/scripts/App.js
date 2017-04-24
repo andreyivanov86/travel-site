0162eff2e459258dd63b362cd8a48a410099d2fd
andreyivanov86@gmail.com <Andreyivanov86@gmail.com>
2017-04-24 13:02:44 -0400

    reveal on scroll features and testestimonials

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,8 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
+import $ from 'jquery';
 
 var mobileMnu = new MobileMenu();
-var revealOnScroll = new RevealOnScroll();
+//var revealOnScroll = new RevealOnScroll();
+new RevealOnScroll($(".feature-item"), "85%");
+new RevealOnScroll($(".testimonial"), "65%");
