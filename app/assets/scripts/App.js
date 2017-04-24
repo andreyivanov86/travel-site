dc2fa08a81eacbe03d193b37b3040d9fa50f928f
andreyivanov86@gmail.com <Andreyivanov86@gmail.com>
2017-04-24 12:23:13 -0400

    features items hidden

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,3 +1,5 @@
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
 
 var mobileMnu = new MobileMenu();
+var revealOnScroll = new RevealOnScroll();
